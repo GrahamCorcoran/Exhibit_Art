a03c87e11994b60f22cf2ccf77a56825b505daba
Graham Corcoran <15987547+Grambles@users.noreply.github.com>
2017-07-29 19:38:50 -0400

    Added json functionality.

--- exhibit_art.py
+++ exhibit_art.py
@@ -1,5 +1,6 @@
 import praw
 import config
+import json
 import time
 
 def login():
@@ -22,6 +23,33 @@ def add_tlc(tlc):
     threadid = tlc.submission
     author = tlc.author
     commentid = tlc
+
+
+def write_to_json(data):
+    with open(config.filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def user_check(author):
+    """
+    :param author: String of a Reddit Username
+    :return: True if author is in json file, else false.
+    """
+    with open("subreddit_data.json", "r") as f:
+        subreddit_data = json.load(f)
+        if author in subreddit_data["Users"]:
+            subreddit_data["Users"][author]["TLC Count"] += 1
+            subreddit_data["Users"][author]["Flair"] = "Testity Test"
+            print("Found!")
+        else:
+            subreddit_data["Users"][author] = {}
+            subreddit_data["Users"][author]["TLC Count"] = 1
+            subreddit_data["Users"][author]["Flair"] = "Testity test"
+            print("Not found, created for next time!")
+        write_to_json(subreddit_data)
+
+user_check("leftpig")
+
 
 
 r = login()
